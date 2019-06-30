9f048c3de1bbf1ab1b1ddc2f26b74afd13278ccf
Caleb K <caleb.kopp@optum.com>
2019-06-30 12:00:44 -0500

    Update DataHandlerTest.java

--- events-api/src/main/java/com/se465/data/DataHandlerTest.java
+++ events-api/src/main/java/com/se465/data/DataHandlerTest.java
@@ -3,9 +3,9 @@ package com.se465.data;
 import static org.junit.Assert.assertTrue;
 
 public class DataHandlerTest {
-	public static void main(String[] args) {
-		String uid = "4188870";
-		assertTrue(DataHandler.getInstance().searchEvent(uid) != null);
-		assertTrue(DataHandler.getInstance().searchEvent(uid).getUid().equals(uid));
-	}
+//	public static void main(String[] args) {
+//		String uid = "4188870";
+//		assertTrue(DataHandler.getInstance().searchEvent(uid) != null);
+//		assertTrue(DataHandler.getInstance().searchEvent(uid).getUid().equals(uid));
+//	}
 }
